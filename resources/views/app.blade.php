8dc2d6b46e70883f8640c7e441dfca604a144ab5
Isaiah Johnson Olamide <olamide3412@gmail.com>
2025-08-15 12:59:05 +0100

    farm logo bug

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/png" href="{{ Vite::asset('resources/images/farm-logo.png') }}">
+        <link rel="icon" type="image/png" href="{{ Vite::asset('resources/images/logo.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
